43c00e94a49691300f345321d30e8faa00c25a86
Lina R <linita1026@hotmail.com>
2021-10-05 19:16:32 -0500

    Actualizacion

---
+++ js/typeNumber.js
@@ -0,0 +1,15 @@
+var miPrimeraVariable = "Hola a todos";
+console.log(isNaN(miPrimeraVariable));
+
+/*
+NaN - Not-A-Number
+*/
+
+miPrimeraVariable = 5;
+console.log(isNaN(miPrimeraVariable));
+
+/* Maximo numero en JS */
+console.log (Number.MAX_VALUE);
+
+/* Minimo numero en JS */
+console.log (Number.MIN_VALUE);
---
+++ js/types.js
@@ -0,0 +1,30 @@
+//javascript es un lenguaje de programación
+//interpretado y no tipado, o debilmente
+//tipado y no es multi-hilo
+//console.log("Hola a todos");
+
+//tipos de variables de js
+//tipo numerico -> var variable = numero;
+var variable = 0;
+console.log(variable);
+//tipo string -> var variable = "texto";
+variable = "Hola a todos";
+console.log(variable);
+//tipo boolean -> var variable = true | false;
+variable = true;
+console.log(variable);
+//tipo objeto -> var variable = {
+//    propiedad: valor;
+//    propiedad2: valor2
+//};
+variable = {};
+console.log(variable);
+//tipo array -> var variable = [valor, valor2, ... , valorn]
+variable = [];
+console.log(variable);
+//tipo null -> var variable = null
+variable = null;
+console.log(variable);
+//tipo undefined -> var variable = undefined
+variable = undefined;
+console.log(variable);
